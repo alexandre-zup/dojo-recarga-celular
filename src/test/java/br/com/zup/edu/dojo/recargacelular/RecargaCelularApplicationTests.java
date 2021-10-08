8a388f68a10f0e4edf1c2383d2d4a09f44883b24
alexandre-zup <alexandre.santos@zup.com.br>
2021-10-08 09:29:40 -0300

    setup

---
+++ src/test/java/br/com/zup/edu/dojo/recargacelular/RecargaCelularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.dojo.recargacelular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecargaCelularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
